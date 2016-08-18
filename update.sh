81106a66c9a905750cd11ef5557d8a81de0f47c2
Alexander Odell <alextrastero@gmail.com>
2016-08-18 10:26:01 +0200

    Fix copying in script

--- update.sh
+++ update.sh
@@ -5,9 +5,9 @@ clear
 echo "Copying react-materialize source into webpack boilerplate"
 echo "Make sure webpack-boilerplate folder is the same path as react-materialize"
 
-SOURCE=$HOME/dev/react-materialize/src
+SOURCE=$HOME/dev/react-materialize/src/
 DEV=$HOME/dev/webpack-boilerplate/src/
 mkdir -p $DEV/react-materialize/
-cp $SOURCE/. $DEV/react-materialize/
+cp -R $SOURCE/. $DEV/react-materialize/
 
 echo $SOURCE
