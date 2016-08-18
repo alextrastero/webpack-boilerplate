72bdafc299cd09e128c0679668d6807458adcf01
Alexander Odell <alextrastero@gmail.com>
2016-08-18 10:23:19 +0200

    Create update script and run in package.json

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+
+echo "Copying react-materialize source into webpack boilerplate"
+echo "Make sure webpack-boilerplate folder is the same path as react-materialize"
+
+SOURCE=$HOME/dev/react-materialize/src
+DEV=$HOME/dev/webpack-boilerplate/src/
+mkdir -p $DEV/react-materialize/
+cp $SOURCE/. $DEV/react-materialize/
+
+echo $SOURCE
