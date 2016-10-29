d81f35ea881feaf3c52e88713bde75ae46831fe3
Alexander Odell <alextrastero@gmail.com>
2016-10-29 11:47:28 +0200

    Fixes some package version issues with hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    'react-hot-loader/patch',
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
     './src/index'
@@ -20,7 +19,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['babel'],
+      loaders: ['react-hot', 'babel'],
       include: path.join(__dirname, 'src')
     }]
   }
