b49757ee36bc4cfba17023a6e0a9324bb7b72b81
Alexander Odell <alextrastero@gmail.com>
2017-02-03 09:33:43 +0100

    Add initial setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,19 @@
-const { addPlugins, createConfig, entryPoint, env, setOutput, sourceMaps, webpack } = require('@webpack-blocks/webpack')
+const { createConfig, env, entryPoint, setOutput, sourceMaps } = require('@webpack-blocks/webpack2')
+const devServer = require('@webpack-blocks/dev-server2')
 const babel = require('@webpack-blocks/babel6')
 const postcss = require('@webpack-blocks/postcss')
-const devServer = require('@webpack-blocks/dev-server')
-const cssnext = require("postcss-cssnext")
-
-const path = require('path')
+const cssnext = require('postcss-cssnext')
 
 module.exports = createConfig([
-  entryPoint('./src'),
-  setOutput({
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist/'
-  }),
-  addPlugins([
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env || 'development')
-    })
-  ]),
+  entryPoint('./src/index.js'),
+  setOutput('./build/bundle.js'),
   babel(),
   postcss([
     cssnext()
   ]),
   env('development', [
-    devServer({
-      port: 3000
-    }),
+    devServer(),
     sourceMaps(),
-    devServer.reactHot(),
-  ]),
+    devServer.reactHot()
+  ])
 ])
