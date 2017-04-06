8e81fb6e29eb1a9d8dfba4b8ba87701b4f48d229
Alexander Odell <alextrastero@gmail.com>
2017-04-06 15:34:48 +0200

    Cleans up the App.js example

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
-export default function App () {
-  console.log('%c This is the App! ', 'font-size: 20px; background: #222; color: #bada55')
+export default () => {
+  console.log(`%cThis is the\nApp!`, 'color: #BADA55')
 }
